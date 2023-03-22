ab12421c2c9ab1844e80b6705c0cc033b2ceeb35
yaproklayt <egor.rigenko828@gmail.com>
2023-03-22 18:19:32 +0500

    /

--- components/VisitsList.php
+++ components/VisitsList.php
@@ -10,18 +10,17 @@
 
         while($item = $Visits->fetch()){
             $item1 = $slots->fetch();
-
+            $spec = $specialists->fetch();
             $client= $clients->fetch();
 
             if($_SESSION['user_type'] == 3){
             if ($item['id_cli'] == $_SESSION['id_auth_user']){
                 if ($item1['id'] == $item['slot_id']){
-
-                    while ($spec= $specialists->fetch()){
-                        if($item1['id_spec'] == $spec['id']){
-                           $name_spec = $spec['nme'];
-                        }
+                    if($spec['id']==$item1['id_spec'] && $item1['id']==$item['slot_id'] ){
+                        $name=$spec['nme'];
                     }
+
+
                     ?>
             <a href="../visit.php?visitId=<?= $item['id']?>" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
                 <div class="d-flex gap-2 w-100 justify-content-between">
@@ -29,7 +28,9 @@
                         <h6 class="mb-0">Дата записи: <?=$item1['day']?> </h6>
                         <p class="mb-0 opacity-75">Время начала встречи: <?=$item1['time_start']?> </p>
 
-                        <p class="mb-0 opacity-75">Ваш специалист: <?=$name_spec?> </p>
+                        <p class="mb-0 opacity-75">id специалистa: <?=$item1['id_spec']?> </p>
+                        <p class="mb-0 opacity-75">id слота: <?=$item['slot_id']?> </p>
+                        <p class="mb-0 opacity-75">ФИО специалиста: <?=$name?> </p>
                     </div>
                     <img src="https://sun9-46.userapi.com/impg/dCBgBPx2KMijvCknZb8vkDovyYo8vAK42HrJoA/HK3rRdU-_DQ.jpg?size=1075x1080&quality=95&sign=6f2961280acced0ee645ce38cbd69133&type=album" alt="twbs" width="64" height="64">
 
@@ -37,8 +38,10 @@
             </a>
 
         <?php }}}
-            else{
-                if($_SESSION['user_type'] == 2){
+
+
+
+        else if($_SESSION['user_type'] == 2){
                     if ($item1['id_spec'] == $_SESSION['id_auth_user']){
                         if ($item1['id'] == $item['slot_id']){
 
@@ -54,7 +57,7 @@
 
             </div>
         </a>
-        <?php }}}}}?>
+        <?php }}}}?>
     </div>
 
 
